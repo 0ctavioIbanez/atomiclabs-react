aea0e1eeddb278d84f183b87657a2c46e5519509
0ctavioibanez <octaviogd@live.com.mx>
2020-08-25 03:36:16 -0500

    Updates

---
+++ precache-manifest.07694ccf2cc4c6c191545228ad27386b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a2083d56d2355e82e7bff6184684be",
+    "url": "/atomiclabs-react/index.html"
+  },
+  {
+    "revision": "09c348c11534b33a34d5",
+    "url": "/atomiclabs-react/static/css/main.0b1821da.chunk.css"
+  },
+  {
+    "revision": "07acb2d841ccf6b90bd9",
+    "url": "/atomiclabs-react/static/js/2.1899c771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/atomiclabs-react/static/js/2.1899c771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c348c11534b33a34d5",
+    "url": "/atomiclabs-react/static/js/main.934965dc.chunk.js"
+  },
+  {
+    "revision": "a03dbc649ec49569e6de",
+    "url": "/atomiclabs-react/static/js/runtime-main.06483c14.js"
+  },
+  {
+    "revision": "664f3dbd69f0e6f359bce2d7433f4e1d",
+    "url": "/atomiclabs-react/static/media/Group_4032.664f3dbd.png"
+  },
+  {
+    "revision": "af1e226642fb10d6637a2cb25543c110",
+    "url": "/atomiclabs-react/static/media/Group_4033.af1e2266.png"
+  },
+  {
+    "revision": "7994bdeb950f94a2dae2f33582bd86b5",
+    "url": "/atomiclabs-react/static/media/Group_4035.7994bdeb.png"
+  },
+  {
+    "revision": "eb5c2125d949bba2719e404125c2b4f1",
+    "url": "/atomiclabs-react/static/media/Group_4036.eb5c2125.png"
+  },
+  {
+    "revision": "ac405c4a1865fdc6422e65e04d630233",
+    "url": "/atomiclabs-react/static/media/Group_4037.ac405c4a.png"
+  },
+  {
+    "revision": "57587e8434968c02a8fb798993654fd1",
+    "url": "/atomiclabs-react/static/media/Group_4040.57587e84.png"
+  },
+  {
+    "revision": "d9d7815d3596aee18b8326e25e09bd1f",
+    "url": "/atomiclabs-react/static/media/Group_4041.d9d7815d.png"
+  },
+  {
+    "revision": "9eaf1b7ae32a26b63966005370062b3b",
+    "url": "/atomiclabs-react/static/media/Group_4042.9eaf1b7a.png"
+  },
+  {
+    "revision": "707215d2d8b5db2e9385f6e491f35e1e",
+    "url": "/atomiclabs-react/static/media/Group_4043.707215d2.png"
+  },
+  {
+    "revision": "ff1a4d4ff92b8d5cd862e894889b1cd6",
+    "url": "/atomiclabs-react/static/media/Mask_Group_1.ff1a4d4f.png"
+  },
+  {
+    "revision": "e8076c13364b9dd2de213243839c7928",
+    "url": "/atomiclabs-react/static/media/footer.e8076c13.jpg"
+  },
+  {
+    "revision": "da84857e64a06e6d459b468c7eab4e4b",
+    "url": "/atomiclabs-react/static/media/logo.da84857e.webp"
+  }
+]);
